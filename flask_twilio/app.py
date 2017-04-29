7e6691dab426967d453454937c79097445ad854c
Gautham Varadarajan <gvaradarajan@vaynermedia.com>
2017-04-29 15:28:17 -0400

    GV - routes_info endpoint

--- flask_twilio/app.py
+++ flask_twilio/app.py
@@ -20,3 +20,23 @@ def message():
 
 def dummy():
     return 'Hello World'
+
+@app.route("/route_info", methods=["GET"])
+
+def route_info():
+    result = {}
+    result['destinations'] = [
+        (36.25977, -119.67269),
+        (36.70476, -120.00366),
+        (36.29741, -119.15634)
+    ]
+    result['fires'] = [
+        # (lat, lon, intensity)
+        (36.52619, -119.57656, 3)
+    ]
+    result['clusters'] = [
+        # (lat, lon, number_of_people),
+        (36.74872, -119.28131, 15)
+    ]
+
+    return jsonify(result)
