ff5ab84bfc0c5e4566c8b7f72b763048dc89100a
Gautham Varadarajan <gvaradarajan@vaynermedia.com>
2017-04-29 18:48:19 -0400

    set up database

--- flask_twilio/app.py
+++ flask_twilio/app.py
@@ -3,8 +3,59 @@
 
 # debug mode for auto restart on file update
 
+import psycopg2
+
 from flask import Flask
 from flask import request, jsonify
+
+try:
+    conn = psycopg2.connect("dbname='smokey' user='smokeyuser' host='localhost' password='onlyyoucanescapeforestfires'")
+    cursor = conn.cursor()
+except Exception as e:
+    print("I am unable to connect to the database")
+    raise e
+
+cursor.execute(
+    """CREATE TABLE IF NOT EXISTS destination (pk SERIAL PRIMARY KEY, latitude integer, longitude integer, UNIQUE(latitude, longitude));"""
+)
+cursor.execute(
+    """CREATE TABLE IF NOT EXISTS person (pk SERIAL PRIMARY KEY, phone_number integer, latitude integer, longitude integer, UNIQUE(phone_number));"""
+)
+cursor.execute(
+    """CREATE TABLE IF NOT EXISTS fire_point (pk SERIAL PRIMARY KEY, latitude integer, longitude integer, intensity integer, day text, time_string text, UNIQUE(latitude, longitude, day, time_string));"""
+)
+cursor.execute(
+    """CREATE TABLE IF NOT EXISTS cluster (pk SERIAL PRIMARY KEY, latitude integer, longitude integer, number_of_people integer, UNIQUE(latitude, longitude));"""
+)
+
+#create database seeds
+cursor.execute("""SELECT COUNT(*) FROM destination;""")
+table_count = cursor.fetchall()
+if table_count[0][0] == 0:
+    cursor.execute(
+        """INSERT INTO destination (latitude, longitude) VALUES (35260, -119673);"""
+    )
+    cursor.execute(
+        """INSERT INTO destination (latitude, longitude) VALUES (36705, -119673);"""
+    )
+    cursor.execute(
+        """INSERT INTO destination (latitude, longitude) VALUES (36297, -119156);"""
+    )
+
+cursor.execute("SELECT COUNT(*) FROM fire_point;")
+table_count = cursor.fetchall()
+if table_count[0][0] == 0:
+    cursor.execute(
+        "INSERT INTO fire_point (latitude, longitude, intensity, day, time_string) VALUES (36297, -119156, 3, '2017-04-28', '08:30PM');"
+    )
+
+
+cursor.execute("SELECT COUNT(*) FROM cluster;")
+table_count = cursor.fetchall()
+if table_count[0][0] == 0:
+    cursor.execute(
+        "INSERT INTO cluster (latitude, longitude, number_of_people) VALUES (36297, -119156, 15)"
+    )
 
 app = Flask(__name__)
 
@@ -25,18 +76,19 @@ def dummy():
 
 def route_info():
     result = {}
+    cursor.execute("SELECT * FROM destination;")
     result['destinations'] = [
-        (36.25977, -119.67269),
-        (36.70476, -120.00366),
-        (36.29741, -119.15634)
+        (latitude / 1000, longitude / 1000) for pk, latitude, longitude in cursor.fetchall()
     ]
+    cursor.execute("SELECT * FROM fire_point;")
     result['fires'] = [
-        # (lat, lon, intensity)
-        (36.52619, -119.57656, 3)
+        (
+            latitude / 1000, longitude / 1000, intensity, day, time
+        ) for pk, latitude, longitude, intensity, day, time in cursor.fetchall()
     ]
+    cursor.execute("SELECT * FROM cluster;")
     result['clusters'] = [
-        # (lat, lon, number_of_people),
-        (36.74872, -119.28131, 15)
+        (latitude / 1000, longitude / 1000, num_people) for pk, latitude, longitude, num_people in cursor.fetchall()
     ]
 
     return jsonify(result)
