f27acc40e014d83a22f243dbf028eae00117dedc
Gautham Varadarajan <gvaradarajan@vaynermedia.com>
2017-04-29 14:42:43 -0400

    GV - dummy data endpoint

--- flask_twilio/app.py
+++ flask_twilio/app.py
@@ -4,7 +4,7 @@
 # debug mode for auto restart on file update
 
 from flask import Flask
-from flask import request
+from flask import request, jsonify
 
 app = Flask(__name__)
 
@@ -16,3 +16,27 @@ def message():
     media_url = request.values.get("MediaUrl0", "")
     return ("<Response><Message>{} from {} url {}</Message></Response>".format(body,number,media_url))
 
+@app.route("/dummy", methods=["GET"])
+
+def dummy():
+    return 'Hello World'
+
+@app.route("/route_info", methods=["GET"])
+
+def route_info():
+    result = {}
+    result['destinations'] = [
+        (36.25977, -119.67269),
+        (36.70476, -120.00366),
+        (36.29741, -119.15634)
+    ]
+    result['fires'] = [
+        # (lat, lon, intensity)
+        (36.52619, -119.57656, 3)
+    ]
+    result['clusters'] = [
+        # (lat, lon, number_of_people),
+        (36.74872, -119.28131, 15)
+    ]
+
+    return jsonify(result)
