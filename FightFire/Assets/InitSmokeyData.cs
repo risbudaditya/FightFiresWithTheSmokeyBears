9d8056731310f560c189c5b436589852a4e5e81c
Darth Scanner <risbud.aditya@gmail.com>
2017-04-30 12:37:00 -0400

    Unity

---
+++ FightFire/Assets/InitSmokeyData.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Net;
+using System.IO;
+
+public class InitSmokeyData : MonoBehaviour {
+
+    private const string URL = "http://10.171.23.160:5000/route_info";
+    //private const string DATA = @"{""object"":{""name"":""Name""}}";
+    private HttpWebRequest request;
+    private StreamWriter requestWriter;
+    private DateTime time;
+    System.Random random;
+
+    // Use this for initialization
+    void Start () {
+        request = (HttpWebRequest)WebRequest.Create(URL);
+        request.Method = "GET";
+        //request.ContentType = "application/json";
+        //request.ContentLength = DATA.Length;
+        requestWriter = new StreamWriter(request.GetRequestStream(), System.Text.Encoding.ASCII);
+        time = DateTime.Now;
+        random = new System.Random();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if ((DateTime.Now - time).Milliseconds > 1000)
+        {
+            return;
+        }
+        time = DateTime.Now;
+        try
+        {
+            //requestWriter.Write(DATA);
+            //requestWriter.Close();
+            WebResponse webResponse = request.GetResponse();
+            Stream webStream = webResponse.GetResponseStream();
+            StreamReader responseReader = new StreamReader(webStream);
+            string response = responseReader.ReadToEnd();
+            SmokeySingleton.Response objResponse = JsonUtility.FromJson<SmokeySingleton.Response>(response);
+            SmokeySingleton.getInstance().fireData = objResponse;
+            transform.Translate(new Vector3(transform.position.x, transform.position.y));
+            Console.Out.WriteLine(objResponse);
+            responseReader.Close();
+        }
+        catch (Exception e)
+        {
+            Console.Out.WriteLine("-----------------");
+            Console.Out.WriteLine(e.Message);
+        }
+    }
+}
---
+++ FightFire/Assets/PeopleStuckInFire.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.IO;
+
+public class PeopleStuckInFire : MonoBehaviour {
+
+    private DateTime time;
+    System.Random random;
+    private static int indexer = 0;
+    private int index;
+
+    // Use this for initialization
+    void Start () {
+        time = DateTime.Now;
+        random = new System.Random();
+        index = ++indexer;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if ((DateTime.Now - time).Milliseconds > 1000) {
+            return;
+        }
+        time = DateTime.Now;
+        int factor = 2;
+        transform.Translate(new Vector3(0.2f - random.Next(1, factor+1) / 10, random.Next(-1 * factor * 2,factor* 2)/3, random.Next(-1*factor, factor) / 10));
+        return;
+        SmokeySingleton.Response fireData = SmokeySingleton.getInstance().fireData;
+        try
+        {
+            float xtransform = (float)(transform.position.x - fireData.clusters[index][0]);
+            float ytransform = (float)(transform.position.y - fireData.clusters[index][1]);
+            ((TextMesh)GetComponent(typeof(TextMesh))).text = fireData.clusters[index][2].ToString();
+            transform.Translate(new Vector3(xtransform, ytransform));
+        }
+        catch (Exception e)
+        {
+            Console.Out.WriteLine("-----------------");
+            Console.Out.WriteLine(e.Message);
+        }
+    }
+}
